5e052c0ee4ccfa5609fd692874725c3bd34f92f4
André Vargas <vargas@geosapiens.com.br>
2016-09-23 17:34:26 -0300

    Initial discipline templates.

--- src/public/app/components/discipline/discipline.edit.controller.js
+++ src/public/app/components/discipline/discipline.edit.controller.js
@@ -1,3 +1,5 @@
 angular.module('hercules').controller('DisciplineEditController', function ($scope) {
 
+
+
 });
--- src/public/app/components/discipline/discipline.new.controller.js
+++ src/public/app/components/discipline/discipline.new.controller.js
@@ -1,3 +1,13 @@
 angular.module('hercules').controller('DisciplineNewController', function ($scope) {
 
+    $scope.discipline = {};
+
+    $scope.discipline.students = [];
+
+    $scope.addStudentField = function () {
+        $scope.discipline.students.push('');
+    }
+
+
+
 });
