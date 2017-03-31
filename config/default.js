25963a77aa3783954dd9f3e7a9cacc7cb1113396
André Vargas <vargas@geosapiens.com.br>
2017-03-31 10:16:17 -0300

    Openshift config

--- config/default.js
+++ config/default.js
@@ -1,6 +1,17 @@
 'use strict';
 
+const host = process.env.NODE_IP || '127.0.0.1';
+const port = process.env.NODE_PORT || 3000;
+
+const db_name = process.env.OPENSHIFT_APP_NAME || 'hercules';
+const mongodb_connection_string = 'mongodb://localhost/' + db_name;
+
+if (process.env.MONGODB_URL) {
+    mongodb_connection_string = process.env.MONGODB_URL + db_name;
+}
+
 module.exports = {
-    port: process.env.NODE_PORT ? process.env.NODE_PORT : 80,
-    mongodb_connection_string: process.env.MONGODB_CONNECTION_STRING ? process.env.MONGODB_CONNECTION_STRING: 'mongodb://localhost/hercules',
+    host: host,
+    port: port,
+    mongodb_connection_string: mongodb_connection_string
 };
